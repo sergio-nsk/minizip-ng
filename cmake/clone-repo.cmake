9c32106827ee54c42d5e43775e91a37a4f59c7f3
Nathan Moinvaziri <nathan@nathanm.com>
2026-02-09 15:04:50 -0800

    Fix for older versions of CMake without FetchContent EXCLUDE_FROM_ALL

--- cmake/clone-repo.cmake
+++ cmake/clone-repo.cmake
@@ -13,23 +13,44 @@ macro(clone_repo name url tag)
     message(STATUS "Fetching ${name} ${${name_upper}_REPOSITORY} ${${name_upper}_TAG}")
 
     # Check for FetchContent cmake support
-    if(${CMAKE_VERSION} VERSION_LESS "3.11")
+    if(${CMAKE_VERSION} VERSION_LESS 3.11)
         message(FATAL_ERROR "CMake 3.11 required to fetch ${name}")
     else()
         include(FetchContent)
 
-        FetchContent_Declare(${name}
-            GIT_REPOSITORY ${${name_upper}_REPOSITORY}
-            GIT_TAG ${${name_upper}_TAG}
-            SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party/${name_lower})
+        # EXCLUDE_FROM_ALL support was added to FetchContent in CMake 3.28.
+        # For older versions, use FetchContent_Populate and add_subdirectory
+        # with EXCLUDE_FROM_ALL manually.
+        if(CMAKE_VERSION VERSION_LESS 3.28)
+            FetchContent_Declare(${name}
+                GIT_REPOSITORY ${${name_upper}_REPOSITORY}
+                GIT_TAG ${${name_upper}_TAG}
+                SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party/${name_lower})
 
-        FetchContent_MakeAvailable(${name})
+            FetchContent_GetProperties(${name})
+            if(NOT ${name_lower}_POPULATED)
+                FetchContent_Populate(${name})
+            endif()
+        else()
+            FetchContent_Declare(${name}
+                GIT_REPOSITORY ${${name_upper}_REPOSITORY}
+                GIT_TAG ${${name_upper}_TAG}
+                SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party/${name_lower}
+                EXCLUDE_FROM_ALL)
 
-        if(NOT ${name_lower}_POPULATED)
-            FetchContent_Populate(${name})
+            FetchContent_MakeAvailable(${name})
         endif()
 
         set(${name_upper}_SOURCE_DIR ${${name_lower}_SOURCE_DIR})
         set(${name_upper}_BINARY_DIR ${${name_lower}_BINARY_DIR})
+
+        # For CMake < 3.28, manually add subdirectory with EXCLUDE_FROM_ALL
+        # if the fetched content has a CMakeLists.txt.
+        # For CMake >= 3.28, FetchContent_MakeAvailable already handles this.
+        if(CMAKE_VERSION VERSION_LESS 3.28)
+            if(EXISTS ${${name_upper}_SOURCE_DIR}/CMakeLists.txt)
+                add_subdirectory(${${name_upper}_SOURCE_DIR} ${${name_upper}_BINARY_DIR} EXCLUDE_FROM_ALL)
+            endif()
+        endif()
     endif()
 endmacro()
