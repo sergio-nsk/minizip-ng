b6bf9914dba8b498e12cc3624e74d1136d804cb5
Nathan Moinvaziri <nathan@nathanm.com>
2026-03-02 16:28:59 -0800

    Add cmake subdirectory support to clone_repo macro for zstd.

--- cmake/clone-repo.cmake
+++ cmake/clone-repo.cmake
@@ -10,6 +10,8 @@ macro(clone_repo name url tag)
         set(${name_upper}_TAG ${tag})
     endif()
 
+    set(_clone_repo_cmake_subdir ${ARGN})
+
     message(STATUS "Fetching ${name} ${${name_upper}_REPOSITORY} ${${name_upper}_TAG}")
 
     # Check for FetchContent cmake support
@@ -32,11 +34,20 @@ macro(clone_repo name url tag)
                 FetchContent_Populate(${name})
             endif()
         else()
-            FetchContent_Declare(${name}
-                GIT_REPOSITORY ${${name_upper}_REPOSITORY}
-                GIT_TAG ${${name_upper}_TAG}
-                SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party/${name_lower}
-                EXCLUDE_FROM_ALL)
+            if(_clone_repo_cmake_subdir)
+                FetchContent_Declare(${name}
+                    GIT_REPOSITORY ${${name_upper}_REPOSITORY}
+                    GIT_TAG ${${name_upper}_TAG}
+                    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party/${name_lower}
+                    SOURCE_SUBDIR ${_clone_repo_cmake_subdir}
+                    EXCLUDE_FROM_ALL)
+            else()
+                FetchContent_Declare(${name}
+                    GIT_REPOSITORY ${${name_upper}_REPOSITORY}
+                    GIT_TAG ${${name_upper}_TAG}
+                    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third_party/${name_lower}
+                    EXCLUDE_FROM_ALL)
+            endif()
 
             FetchContent_MakeAvailable(${name})
         endif()
@@ -48,8 +59,12 @@ macro(clone_repo name url tag)
         # if the fetched content has a CMakeLists.txt.
         # For CMake >= 3.28, FetchContent_MakeAvailable already handles this.
         if(CMAKE_VERSION VERSION_LESS 3.28)
-            if(EXISTS ${${name_upper}_SOURCE_DIR}/CMakeLists.txt)
-                add_subdirectory(${${name_upper}_SOURCE_DIR} ${${name_upper}_BINARY_DIR} EXCLUDE_FROM_ALL)
+            set(_clone_repo_cmake_dir ${${name_upper}_SOURCE_DIR})
+            if(_clone_repo_cmake_subdir)
+                set(_clone_repo_cmake_dir ${_clone_repo_cmake_dir}/${_clone_repo_cmake_subdir})
+            endif()
+            if(EXISTS ${_clone_repo_cmake_dir}/CMakeLists.txt)
+                add_subdirectory(${_clone_repo_cmake_dir} ${${name_upper}_BINARY_DIR} EXCLUDE_FROM_ALL)
             endif()
         endif()
     endif()
